6636bb488fee0110a0cb2cc3b326e9e719a1e713
Scott Diallo <diallot@me.com>
2016-07-13 11:00:17 -0700

    gif images added for gender selection && td styled for better look

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$(function () {
+    //console.log("hello world!");
+
+    $(".tableContent").hide();
+    $(".gameForm").hide();
+
+    $(".gender").on('click', function () {
+        $(".tableContent").show();
+        $(".gameForm").show();
+        //any gender chosen hide gender box and display gif image appropriate to gender selection
+    });
+
+
+
+
+
+});
