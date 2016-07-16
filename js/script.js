b760f15ef287f22dc32eb70be50b46fbe46d9b45
Scott Diallo <diallot@me.com>
2016-07-16 16:49:25 -0700

    ng-init applied

--- js/script.js
+++ js/script.js
@@ -1,19 +1,7 @@
-$(function () {
-    //console.log("hello world!");
+var app = angular.module('myApp', ['ngMessages'])
+app.constant('VERSION', '1.0')
+app.run(function (VERSION, $rootScope) {
+    $rootScope.version = VERSION;
+})
 
-    $(".tableContent").hide();
-    $(".gameForm").hide();
-
-    $(".gender").on('click', function () {
-        $(".tableContent").show();
-        $(".gameForm").show();
-        //any gender chosen hide gender box and display gif image appropriate to gender selection
-    });
-
-    $('.submit').on('click', function () {
-        //alert('hello world!');
-    })
-
-
-
-});
+//modify ng-model to include data.name etc...
