3a5e0624df44fae2fd21916f638e6fcb9202f750
Scott Diallo <diallot@me.com>
2016-07-17 23:40:37 -0700

    controller added

--- js/script.js
+++ js/script.js
@@ -1,7 +1,20 @@
 var app = angular.module('myApp', ['ngMessages'])
-app.constant('VERSION', '1.0')
+app.constant('VERSION', "1.0")
 app.run(function (VERSION, $rootScope) {
     $rootScope.version = VERSION;
 })
 
+.controller("myCtrl", function ($scope) {
+    $scope.data = {};
+    $scope.gender = {};
+    $scope.inputHide = false;
+    $scope.submit = function () {
+        $scope.inputHide = true;
+    }
+    $scope.reset = function () {
+        $scope.inputHide = false;
+        $scope.data = {};
+        location.reload();
+    }
+});
 //modify ng-model to include data.name etc...
